d9d6aaa4eb2194dcf5c8afd2ab11f3f01672114f
Cory DeGuzman <deguzmancory@ymail.com>
2024-04-10 19:24:42 -0700

    [TA-15]: Fixed a bug where FastAPI container would try to connect before Postgres container was fully initialized.

--- Backend/queries/pool.py
+++ Backend/queries/pool.py
@@ -1,9 +1,24 @@
 import os
 from psycopg2 import pool
+import time
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-connection_pool = pool.SimpleConnectionPool(1, 10, dsn=DATABASE_URL)
 
-if connection_pool:
-    print("Connection pool created successfully")
+def create_connection_pool(max_retries=3, retry_delay=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL)
+            if connection_pool:
+                print("Connection pool created successfully")
+                return connection_pool
+        except Exception as e:
+            print(f"Error creating connection pool: {e}")
+            retries += 1
+            time.sleep(retry_delay)
+    print("Max retries reached. Connection pool creation failed")
+    return None
+
+
+connection_pool = create_connection_pool()
