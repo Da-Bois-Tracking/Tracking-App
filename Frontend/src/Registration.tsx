e1235a064bbd10f584a5b05b40701d95ad35f9d2
Lance Amolo <lanceamolo808@yahoo.com>
2024-04-20 01:01:06 -0700

    Form started

---
+++ Frontend/src/Registration.tsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+import { CountryDropdown, StateDropdown, CityDropdown, PhoneInput } from "react-country-state-dropdown";
+
+// interface User {
+//     userName: string
+// }
+
+const Registration = () => {
+     const [ userName, setUserName ] = useState("")
+     const [ firstName, setFirstName ] = useState("")
+     const [ lastName, setLastName ] = useState("")
+     const [ email, setEmail ] = useState("")
+     const [ password, setPassword ] = useState("")
+     const [ confirmPassword, setConfirmPassword ] = useState("")
+     const [ dob, setDob ] = useState("")
+     const [ phoneNumber, setPhoneNumber ] = useState("")
+     const [ country, setCountry ] = useState("")
+     const [ state, setState ] = useState("")
+     const [ city, setCity ] = useState("")
+     const [ weight, setWeight ] = useState("")
+     const [ height, setHeight ] = useState("")
+     const [ skillLevel, setSkillLevel ] = useState("")
+     const [ position, setPosition ] = useState("")
+    
+
+
+     return (
+        <div>
+            <h1>Regsistration</h1>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="Username"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)} />
+                <input 
+                    type="text" 
+                    placeholder="First Name"
+                    value={firstName} 
+                    onChange={(e) => setFirstName(e.target.value)} />
+                <input 
+                    type="text" 
+                    placeholder="Last Name"
+                    value={lastName} 
+                    onChange={(e) => setLastName(e.target.value)} />
+                <input 
+                    type="email" 
+                    placeholder="Email"
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} />
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} />
+                <input 
+                    type="password" 
+                    placeholder="Confirm Password"
+                    value={confirmPassword} 
+                    onChange={(e) => setConfirmPassword(e.target.value)} />
+                <label htmlFor="dob">Date of Birth: </label>
+                <input 
+                    type="date" 
+                    value={dob} 
+                    onChange={(e) => setDob(e.target.value)} />
+                <PhoneInput 
+                    clearable 
+                    country={country} 
+                    value={phoneNumber} 
+                    onChange={(e) => setPhoneNumber(e.target.value)} />
+                <CountryDropdown 
+                    clearable 
+                    value={country} 
+                    onChange={(e) => setCountry(e.target.value)} />
+                <StateDropdown 
+                    clearable 
+                    country={country} 
+                    value={state} 
+                    onChange={(e) => setState(e.target.value)} />
+                <CityDropdown 
+                    clearable 
+                    allowFreeFormText 
+                    country={country}
+                    state={state} 
+                    value={city} onChange={(e) => setCity(e.target.value)} />
+                <input 
+                    type="number" 
+                    placeholder="Weight(kg)"
+                    value={weight} 
+                    onChange={(e) => setWeight(e.target.value)} />
+                <input 
+                    type="number" 
+                    placeholder="Height(cm)"
+                    value={height} 
+                    onChange={(e) => setHeight(e.target.value)} />
+            </form>
+        </div>
+     )
+}
+
+export default Registration
